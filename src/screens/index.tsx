3ec7574066025814eff738b5aab5495e0db173f2
Tamires <79336444+tamicristina@users.noreply.github.com>
2022-11-09 09:21:10 -0300

    adjust screen style

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,15 +1,27 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { Header } from "../components/Header";
-import { Input } from "../components/TextInput";
 import { styles } from "./styles";
+import { Image } from "react-native";
 
 export default function Home() {
   return (
     <>
       <View style={styles.container}>
         <Header />
-        <Input />
+        <View style={styles.inputAndButtonContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Adicione uma nova tarefa"
+            placeholderTextColor="#808080"
+          />
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Image
+              style={styles.buttonImage}
+              source={require("../../assets/button.png")}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
     </>
   );
--- src/screens/styles.ts
+++ src/screens/styles.ts
@@ -1,9 +1,36 @@
-import { StyleSheet } from "react-native";
+import { Button, StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#1A1A1A",
+  },
+
+  inputAndButtonContainer: {
+    flexDirection: "row"
+  },
+
+  input: {
+    backgroundColor: "#262626",
+    height: 56,
+    width: 271,
+    borderRadius: 6,
+    marginLeft: 24,
+    marginRight: 4,
+    padding: 16,
+  },
+  buttonContainer: {
+    backgroundColor: "#1E6F9F",
+    width: 52,
+    height: 52,
+    borderRadius: 6,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonImage: {
+    width: 16
   }
+
+
 
 })
