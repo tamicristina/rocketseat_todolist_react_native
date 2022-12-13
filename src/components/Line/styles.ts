94d2a465f6ba9ba850b7fce893812943831bf36e
Tamires <79336444+tamicristina@users.noreply.github.com>
2022-12-13 20:32:27 -0300

    create styles for line

---
+++ src/components/Line/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  lineContainer: {
+    alignItems: "center",
+    paddingTop: 20,
+  },
+  line: {
+    borderBottomWidth: 0.3,
+    borderBottomColor: "#D9D9D9",
+    opacity: 0.5,
+    backgroundColor: "#4EA8DE",
+    width: 327,
+  },
+});
